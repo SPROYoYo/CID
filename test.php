1e96af9feb6cd946edf7de70e6feda5b6d9a4a41
YoYoXC <43702866+YoYoXC@users.noreply.github.com>
2020-10-14 21:22:48 +0300

    Update test.php

--- test.php
+++ test.php
@@ -6,5 +6,10 @@ $last_id = "srfl7PnoaQE";
 
 # this function will generate a new fresh ID
 echo CID::NEW($last_id); 
+
 # The new ID generated will be: srfl7PnoaQF
+
+
+# If the generated IDs will reach the maximum point of combinations (ZZZZZ...) it will add a new bit at the string:
+# e.g: ZZX -> ZZZ -> 0000 -> 0001 ....
 ?>
