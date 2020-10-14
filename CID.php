7d10e4f8047d4a768665c6ac4b4c90fe0e559cfb
YoYoXC <43702866+YoYoXC@users.noreply.github.com>
2020-10-14 20:59:47 +0300

    Update CID.php

--- CID.php
+++ CID.php
@@ -13,41 +13,47 @@ class CID {
 	}
 	
 	public static function NEW($last_key_used) {
-		$split = str_split(strrev($last_key_used));
-		$new_key = array();
-		$plus = true;
-		$count_last = 0;
-		for($i=0;$i<=count($split)-1;$i++) {
-			
-			if(!in_array($split[$i], self::$array)) {
-				return 'Oops! I think there are invalid characters in the string provided. Can you check, please?';
-				break;
-			}
-			
-			$this_char = $last_char = $split[$i];
-			
-			if(self::place($this_char) === (count(self::$array)-1)) {
-				$count_last++;
-			}
-			
-			if($plus) {
-				$plus = false;
-				if(self::place($this_char) === (count(self::$array)-1)) {
-					$this_char = self::$array[0];
-					$plus = true;
-				}
-				else {
-					$this_char = self::$array[self::place($this_char)+1];
-				}
-			}
-			$new_key[] = $this_char;
-			
-			if($i == (count($split)-1) && self::place($last_char) === (count(self::$array)-1) && $count_last === count($split)) {
-				$new_key[] = self::$array[0];
-			}
-			
+		if($last_key_used === NULL) {
+			$CID = self::$array[0];
 		}
-		return strrev(implode($new_key));
+		else {
+			$split = str_split(strrev($last_key_used));
+			$new_key = array();
+			$plus = true;
+			$count_last = 0;
+			for($i=0;$i<=count($split)-1;$i++) {
+				
+				if(!in_array($split[$i], self::$array)) {
+					return 'Oops! I think there are invalid characters in the string provided. Can you check, please?';
+					break;
+				}
+				
+				$this_char = $last_char = $split[$i];
+				
+				if(self::place($this_char) === (count(self::$array)-1)) {
+					$count_last++;
+				}
+				
+				if($plus) {
+					$plus = false;
+					if(self::place($this_char) === (count(self::$array)-1)) {
+						$this_char = self::$array[0];
+						$plus = true;
+					}
+					else {
+						$this_char = self::$array[self::place($this_char)+1];
+					}
+				}
+				$new_key[] = $this_char;
+				
+				if($i == (count($split)-1) && self::place($last_char) === (count(self::$array)-1) && $count_last === count($split)) {
+					$new_key[] = self::$array[0];
+				}
+				
+			}
+			$CID = strrev(implode($new_key));
+		}
+		return $CID;
 	}
 }
 ?>
