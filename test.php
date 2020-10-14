88c5c215566df88a4bb4d6d58adc928fefc6f75f
YoYoXC <43702866+YoYoXC@users.noreply.github.com>
2020-10-14 06:32:05 +0300

    Update test.php

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ include 'CID.php';
 # this will be the last ID used in your project.
 $last_id = "srfl7PnoaQE";
 
-# here this nice function will generate a new fresh ID
-echo CID:NEW($last_id);
+# this function will generate a new fresh ID
+echo CID::NEW($last_id); 
+# The new ID generated will be: srfl7PnoaQF
 ?>
